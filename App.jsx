ce44501e468028a83f4355f2d871fdfd8424b867
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-07-26 01:17:11 +0530

    Update App.jsx
    Added New function component and sent get request to get ID number from small set of data , inside App I used get request to get large set of data . For both i used axios library ANd a free website which allow to send any kind of request  , really helpful to make our skills sharp and practicle , 
    Link of website is below 
    https://reqres.in/

--- App.jsx
+++ App.jsx
@@ -20,9 +20,11 @@ function App() {
       <ul>
         {users.map(user => (
           <li key={user.id}>
+            <h2>{user.id}</h2>
+            <h2>{user.first_name} {user.last_name}</h2>
             <img src={user.avatar} alt={`${user.first_name} ${user.last_name}`} />
-            <p>{user.first_name} {user.last_name}</p>
             <p>{user.email}</p>
+            <p>{user.email}</p> {/* Second occurrence of the email */}
           </li>
         ))}
       </ul>
@@ -30,4 +32,30 @@ function App() {
   );
 }
 
-export default App;
+
+
+
+function New() {
+  const [user,setUsers] = useState([])
+
+  useEffect(() => {
+    axios.get('https://reqres.in/api/users/2')
+    .then(response => {
+      setUsers(response.data.data)
+    })
+    .catch(error => {
+      console.error('Error fetching data:', error);
+    });
+  },[])
+
+  return (
+    <div>
+      <h1>GET REQUEST SECOND STARTS FROM HERE</h1>
+      <p1>{user.id}</p1>
+
+    </div>
+
+  )
+}
+
+export {App,New};
