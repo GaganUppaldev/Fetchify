b611d4b0f48ed5b4cf1a1146111d7c4f8c7c8c69
GaganUppaldev <149155501+GaganUppaldev@users.noreply.github.com>
2024-07-26 20:00:55 +0530

    Update index.js
    imported and rendered new function component PostRequestComponent import React from App.jsx which was sending post http request to server

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {App,New} from "./App.jsx";
+import {App,New,PostRequestComponent} from "./App.jsx";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
     <React.StrictMode>
         <App></App>
         <New></New>
+        <PostRequestComponent></PostRequestComponent>
     </React.StrictMode>
     );
